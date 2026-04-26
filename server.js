4cb16d08845e4fc6f3869d629620244c2f3f68b3
sayan | NotDev <85256833+sayan20004@users.noreply.github.com>
2026-04-26 07:49:15 +0530

    render

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER || 'tm.zendo@gmail.com',
-    pass: process.env.EMAIL_PASSWORD
-,  },
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 // Routes
